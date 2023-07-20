7a7554b2f5ac41d1f24e08cd7632ca25822b4e33
rafayelgalstyan1 <rafayel.galstyan1@tumo.org>
2023-07-20 17:58:22 +0400

    random football temas pictures

--- main.js
+++ main.js
@@ -4,55 +4,72 @@ let incorrectAnswer = 0;
 
 
 function getElement(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
-function timer() {
-    getElement("time").innerHTML = seconds;
-    let countdown = setInterval(function () {
-        seconds--;
-        getElement("time").textContent = seconds;
-        if (seconds <= 0) {
-            clearInterval(countdown);
+function getRandomCountry() {
+  return countries[Math.floor(Math.random(countries.lenght - 1) * 10)]
+}
 
-        }
-        if (seconds === 8) {
-            getElement("time").style.color = "blue"
-        }
-        if (seconds === 29) {
-            getElement("time").style.color = "green"
-        }
-        if (seconds === 20) {
-            getElement("time").style.color = "yellow"
-        }
-        if (seconds === 5) {
-            getElement("time").style.color = "red"
-        }
-    }, 1000);
+function main() {
+  coun = getRandomCountry();
+
+
+  getElement("footballteam").src = coun.team;
+  getElement("optionlabel").innerHTML = coun.name;
+
+
+
+}
+
+
+function timer() {
+  getElement("time").innerHTML = seconds;
+  let countdown = setInterval(function () {
+
+    main();
+    seconds--;
+    getElement("time").textContent = seconds;
+    if (seconds <= 0) {
+      clearInterval(countdown);
+
+    }
+    if (seconds === 8) {
+      getElement("time").style.color = "blue"
+    }
+    if (seconds === 29) {
+      getElement("time").style.color = "green"
+    }
+    if (seconds === 20) {
+      getElement("time").style.color = "yellow"
+    }
+    if (seconds === 5) {
+      getElement("time").style.color = "red"
+    }
+  }, 1000);
 }
 
 timer();
 
 function check() {
-    let input;
-    try {
-      input = document.querySelector('input[name = "option"]:checked').value;
-    } catch {
-      return;
-    }
-    if (input === "Հայաստան") {
-      correctAnswer++;
-      getElement("score").innerHTML = correctAnswer;
-    } else {
-      incorrectAnswer++;
-    }
-    clearInterval(checkInterval);
+  let input;
+  try {
+    input = document.querySelector('input[name = "option"]:checked').value;
+  } catch {
+    return;
   }
-  function finish() {
-    clearInterval(checkInterval);
-    let percentage = (correctAnswer / (correctAnswer+incorrectAnswer)) *100;
-    getElement("alertaccuracy").innerHTML = ` qo ardyunqn e ${percentage}%`;
+  if (input === "Հայաստան") {
+    correctAnswer++;
+    getElement("score").innerHTML = correctAnswer;
+  } else {
+    incorrectAnswer++;
   }
-  let checkInterval = setInterval(check, 50);
-  timer();
+  clearInterval(checkInterval);
+}
+function finish() {
+  clearInterval(checkInterval);
+  let percentage = (correctAnswer / (correctAnswer + incorrectAnswer)) * 100;
+  getElement("alertaccuracy").innerHTML = ` qo ardyunqn e ${percentage}%`;
+}
+let checkInterval = setInterval(check, 50);
 
--- team.js
+++ team.js
@@ -4,23 +4,122 @@ const countries = [
         "name": "Արսենալ"
     },
     {
-        "team": "./images/Arsenal_FC.svg",
+        "team": "./images/download-1.jpg",
         "name": "Բարնլի"
     },
     {
-        "team": "./images/Arsenal_FC.svg",
+        "team": "./images/download-1.png",
         "name": "Չելսի"
     },
     {
-        "team": "./images/Arsenal_FC.svg",
-        "name": "Արսենալ"
+        "team": "./images/download-2.jpg",
+        "name": "Ալաշկերտ"
     },
     {
-        "team": "./images/Arsenal_FC.svg",
-        "name": "Արսենալ"
+        "team": "./images/download-2.png",
+        "name": "Մանչեսթեր Սիթի"
     },
     {
-        "team": "./images/Arsenal_FC.svg",
-        "name": "Արսենալ"
+        "team": "./images/download-3.jpg",
+        "name": "Սևան"
     },
-  ]
+    {
+        "team": "./images/download-3.png",
+        "name": "Ասթոն Վիլա"
+    },
+    {
+        "team": "./images/download-4.png",
+        "name": "Բրենթֆորդ"
+    },
+    {
+        "team": "./images/download-5.png",
+        "name": "Բրայթոն"
+    }, {
+        "team": "./images/download-6.png",
+        "name": "Լիվերպուլ"
+    },
+    {
+        "team": "./images/download-7.png",
+        "name": "Տոտենհեմ"
+    },
+    {
+        "team": "./images/download-8.png",
+        "name": "Էվերթոն"
+    },
+    {
+        "team": "./images/download-9.png",
+        "name": "Բարսելոնա"
+    }, {
+        "team": "./images/download-10.png",
+        "name": "Ռեալ Մադրիդ"
+    },
+    {
+        "team": "./images/download-11.png",
+        "name": "Ատլետիկո Մադրիդ"
+    },
+    {
+        "team": "./images/download-12.png",
+        "name": "Ռոմա"
+    },
+    {
+        "team": "./images/download-13.png",
+        "name": "Ինտեր"
+    },
+    {
+        "team": "./images/download-14.png",
+        "name": "Ինտեր Մայամի"
+    },
+    {
+        "team": "./images/download-15.png",
+        "name": "Միլան"
+    },
+    {
+        "team": "./images/download-16.png",
+        "name": "Յուվենտուս"
+    },
+    {
+        "team": "./images/download-17.png",
+        "name": "Փյունիք"
+    },
+    {
+        "team": "./images/download-18.png",
+        "name": "Արարատ Արմենիա"
+    },
+    {
+        "team": "./images/download-18.png",
+        "name": "Արարատ Արմենիա"
+    },
+
+    {
+        "team": "./images/download-19.png",
+        "name": "Արարատ"
+    },
+    {
+        "team": "./images/download-20.png",
+        "name": "ֆուլհեմ"
+    },
+    {
+        "team": "./images/download-21.png",
+        "name": "Վոլվես"
+    },
+    {
+        "team": "./images/download-22.png",
+        "name": "Նյուքասլ Յունայթեդ"
+    },
+    {
+        "team": "./images/download-23.png",
+        "name": "Ատալանտա"
+    },
+    {
+        "team": "./images/download-24.png",
+        "name": "Նապոլի"
+    },
+    {
+        "team": "./images/download.jpg",
+        "name": "Բորնմութ"
+    },
+    {
+        "team": "./images/download.png",
+        "name": "Մանչեսթեր Յունայթեդ"
+    },
+]
